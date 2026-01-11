2c76493ac53c8d430b08aa5b707740981f02bd96
Student <foobar@sr.htlweiz.at>
2026-01-11 21:44:32 +0100

    h

---
+++ Mylibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace Mylibrary;
+
+public class Class1
+{
+    public static int Binary_converter(string binary){
+
+        return Convert.ToInt32(binary, 2);
+    }
+   
+    
+    
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Mylibrary;
+using System;
+namespace program;
+public class program00{
+    static void Main(){
+
+        Console.Write("Geben sie eine Binaerzahl ein:");
+        string binary = Console.ReadLine();
+        int dec_value = Class1.Binary_converter(binary);
+        Console.WriteLine(dec_value);
+    }
+}
