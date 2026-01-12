10e585d09d20081c26d9bf01cbf6e7ee9bea6a6f
Student <foobar@sr.htlweiz.at>
2026-01-12 09:46:22 +0100

    h

--- Mylibrary/Class1.cs
+++ Mylibrary/Class1.cs
@@ -2,7 +2,7 @@
 
 public class Class1
 {
-    public static int Binary_converter(string binary){
+    public static int Binary_converter(string? binary){
 
         return Convert.ToInt32(binary, 2);
     }
